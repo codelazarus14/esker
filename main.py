9ec70f2c44de6c087915100382541d95faf9d53e
codelazarus14 <codel1763@gmail.com>
2022-05-19 20:47:51 -0400

    Cleared old esker stuff

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import random
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_BOT_SECRET')  # secret token for bot to run
+TOKEN = os.getenv('RYTHM_BOT_SECRET')  # secret token for bot to run
 BOT_PREFIX = ['~', 'e.']  # prefixes for bot slash commands
 
 # TODO: make Cogs for different slash command types: https://docs.pycord.dev/en/master/ext/commands/cogs.html
@@ -37,64 +37,7 @@ async def hello(context):
         'Nice lunar weather we\'re having...',
         'Welcome to the Lunar Outpost! I don\'t get visitors very often...'
     ]
-    await context.send(chat_styler(random.choice(responses)))
-
-
-@client.command(name='dialogue',
-                description='Relive your favorite bits of Mobius Digital Esker Dialogue™.',
-                brief='Chat with Esker',
-                aliases=['dialog'],
-                pass_context=True
-                )
-async def dialogue(context):
-    """ TODO: figure out how to recreate dialogue system
-    this is honestly such an entangled mess to work through, and it might not pay off
-    if we just end up using ephemeral messages to have 1-on-1s with each participant.
-    could lucabot it - randomly spit out favorite esker quotes in the meantime"""
-    # dtree = DialogueTree(dialogue.NODES, 'start')
-    # await dtree.evaluate(dtree.nodes[0])
-    await context.send("sorry pal i gotta develop a dialogue system first")
-
-
-@client.command(name='mallow',
-                description='Toast a marshmallow to your liking, or even burn it to a crisp.',
-                brief='Toast a marshmallow',
-                aliases=['marshmallow'],
-                pass_context=True
-                )
-async def mallow(context):
-    # marshmallow minigame w pixel art or maybe just numbers for now
-    await context.send(chat_styler("NO MALLOWS FOR YOU!"))
-
-
-@client.command(name='tunes',
-                description='Wanna listen to some tunes?',
-                brief='Do you hear music?',
-                aliases=['music', 'tune'],
-                pass_context=True
-                )
-async def tunes(context):
-    # Reply with a random OST link if in text chat
-    # in voice, put YouTube playlist of OST on shuffle
-    await context.send(chat_styler("Looks like the tape player is broken... again. Maybe I can ask Hornfels about it "
-                                   "the next time they check up on me. They oughta be able to get Slate's engineering "
-                                   "genius on board."))
-
-
-@client.command(name='rock_assn',
-                description='Discover your true Hearthian name!',
-                brief='Assigned rock at slash command',
-                aliases=['rock', 'rock_assignment', 'ra'],
-                pass_context=True
-                )
-async def rock_assn(context):
-    # Hash their username? and then use algorithm to decide
-    # what their rock is from a list of possibilities
-
-    # Lists are indexed in python - just dynamic arrays really
-    rocks = ["Granite"]
-    rock_index = 0  # generate from hash function
-    await context.send(chat_styler("I reckon you'd make a fine " + rocks[rock_index] + ", hatchling!"))
+    await context.send(random.choice(responses))
 
 
 def chat_styler(text):
