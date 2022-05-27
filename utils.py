2c622946bad74a85aae56a313ac94f5bba1c3589
codelazarus14 <codel1763@gmail.com>
2022-05-27 12:35:14 -0400

    Started setting up embeds

--- utils.py
+++ utils.py
@@ -1,4 +1,30 @@
-def chat_styler(text):
-    """used to style text for fancy chat output"""
-    # TODO: redesign chat styler to be mobile-friendly.. embed?
-    return "**\n" + text + "**\n"
+import json
+
+import discord.ext
+
+
+class BadEmbedTypeError(ValueError):
+    """Error on bad embed/format type given to make_embed()"""
+
+
+def make_embed(embed_type: int, cog: discord.ext.commands.Cog, context: discord.ext.commands.Context) -> discord.Embed:
+    """Creates an embed for putting in chat given a format indicator
+    that should match the context from which make_embed() is called
+
+    | See type_to_file in this method but really this is just my own utility method"""
+
+    type_to_file = {
+
+    }
+    if embed_type not in type_to_file:
+        raise BadEmbedTypeError
+    # find corresponding json file with dict
+    with open(f'json/{type_to_file[embed_type]}.json', 'r') as embed_json:
+        embed_dict = json.load(embed_json)
+        # convert dict to embed
+        embed = discord.Embed.from_dict(embed_dict)
+        embed.set_author()
+        # update each template with data from context
+        match embed_type:
+            case _:
+                return embed
